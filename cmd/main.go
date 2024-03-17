2ef764b9f8b0cd153ec803d22989c588bb0c3086
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2024-03-17 23:00:59 +0100

    implementing the cmd parsing
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -3,4 +3,47 @@
 // / Author: Vincenzo Palazzo <vincenzopalazzo@member.fsf.org>
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/akamensky/argparse"
+)
+
+type CmdParser struct {
+	parser  *argparse.Parser
+	dataDir *string
+	network *string
+}
+
+func (self *CmdParser) Parse(args []string) error {
+	return self.parser.Parse(args)
+}
+
+func (self *CmdParser) Usage(error error) string {
+	return self.parser.Usage(error)
+}
+
+func buildCmdParser() (*CmdParser, error) {
+	parser := argparse.NewParser("lnprototestd", "Lightning Network protocol test Deamon (I am a simple proxy)")
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+	dataDir := parser.String("d", "data-dir", &argparse.Options{Required: false, Default: home, Help: "data directory for ocean market deamon"})
+	_ = parser.Int("p", "port", &argparse.Options{Required: false, Default: 9090, Help: "Default port where the liquidity provider will listen"})
+	_ = parser.String("a", "address", &argparse.Options{Required: false, Default: "127.0.0.1", Help: "Host where the liquidity provider will listen about new connection"})
+	network := parser.String("n", "network", &argparse.Options{Required: false, Default: "testnet", Help: "The Bitcoin Network"})
+	return &CmdParser{parser: parser, dataDir: dataDir, network: network}, nil
+}
+
+func main() {
+	parser, err := buildCmdParser()
+	if err != nil {
+		panic(fmt.Errorf("%s", err))
+	}
+
+	if err := parser.Parse(os.Args); err != nil {
+		panic(parser.Usage(err))
+	}
+}
